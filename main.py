5911b4c4caa99f730323c53cf0fe9246ccf09777
Connor McDermid <connor.mcdermid@mcvts.org>
2022-09-28 23:15:37 -0400

    Finished guesser feature
    Signed-off-by: Connor McDermid <connor.mcdermid@mcvts.org>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv  # take environment vars like API token from .env file
 import asyncio
+import random
 
 env_path = join(dirname(__file__), '.env')
 load_dotenv(env_path)
@@ -11,18 +12,45 @@ TOKEN = os.environ.get("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = discord.Bot(intents=intents)
 
-@client.event
+last_random_message = ""
+
+
+@bot.event
 async def on_ready():
-    print(f'Logged on as {client.user}!')  # print a message when the bot comes online
+    print(f'Logged on as {bot.user}!')  # print a message when the bot comes online
 
-@client.event
+
+@bot.event
 async def on_message(message):
-    print(f'Message from {message.author}: {message.content}')  # let me know when the bot sees a message
+    if message.content.startswith('c!'):
+        await message.channel.send("Acknowledged.")
+
 
 async def send(message):
-    await client.get_channel(1000612336593285174).send(message)
+    await bot.get_channel(1000612336593285174).send(message)
 
 
-client.run(TOKEN)
+@bot.slash_command(name="test", guild_ids=[1000612336593285171])
+async def test(ctx):
+    await ctx.respond("ACK")
+
+
+@bot.slash_command(name="msguessr", guild_ids=[1000612336593285171])
+async def msguessr(ctx):
+    await ctx.respond(f"Acknowledged. Give me a second to prepare...")
+    print("Command received. Indexing...")
+    messages = await ctx.channel.history(limit=None).flatten()
+    random_message = random.choice(messages)
+    global last_random_message
+    last_random_message = random_message
+    print("done.")
+    await ctx.send(f"Who sent this message?: {random_message.content}")
+
+
+@bot.slash_command(name="answer", guild_ids=[1000612336593285171])
+async def answer(ctx):
+    await ctx.respond(last_random_message.author)
+
+bot.run(TOKEN)
